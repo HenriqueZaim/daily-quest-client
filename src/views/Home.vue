8fa5b3b448c64b525a9c0511e3090b8fce6a9b78
Henrique Zaim <henriquezsanches@gmail.com>
2021-01-18 14:00:59 -0300

    :tada: Projeto criado e configurado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section id="apresentation">
+      <v-app-bar
+
+        color="transparent"
+        dark
+        flat
+        py-4
+        fixed
+      >
+        <v-container grid-list-xs>
+
+          <v-btn
+            href="https://github.com/vuetifyjs/vuetify/releases/latest"
+            target="_blank"
+            text
+          >
+            <span class="mr-2">Latest Release</span>
+            <v-icon>mdi-open-in-new</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+
+          <v-btn
+            outlined
+            tile
+          >
+            Cadastre-se
+          </v-btn>
+        </v-container>
+      </v-app-bar>
+    </section>
+    <section>
+
+    </section>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style lang="scss" scoped>
+  #apresentation{
+    min-height: 70vh;
+    background-size: cover;
+    background-image: linear-gradient(),url('../img/wallpaper.jpg');
+  }
+</style>
